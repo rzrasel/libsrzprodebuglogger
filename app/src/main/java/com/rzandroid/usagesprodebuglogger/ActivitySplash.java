ad086517a905ccc93daf0e267c1ee189d6983f2c
Rz Rasel <emailrzrasel@gmail.com>
2022-09-18 15:30:33 +0600

    Project create and initialization

--- app/src/main/java/com/rzandroid/usagesprodebuglogger/ActivitySplash.java
+++ app/src/main/java/com/rzandroid/usagesprodebuglogger/ActivitySplash.java
@@ -2,13 +2,23 @@ package com.rzandroid.usagesprodebuglogger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
+import com.rzandroid.libsprodebuglogger.ProLogWriter;
+
 public class ActivitySplash extends AppCompatActivity {
+    private Activity activity;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        activity = this;
+        context = this;
         setContentView(R.layout.activity_splash);
+        //
+        ProLogWriter.log("DEBUG_LOG_WRITE");
     }
 }
